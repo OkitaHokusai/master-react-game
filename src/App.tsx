e4111bd59680dfeeb74a8b02a132660724f26ff7
OkitaHokusai <21800014@aluno.cotemig.com.br>
2021-05-18 13:32:11 -0300

    redux setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 
 import GameArea from './components/GameArea';
+import allReducers from './reducers';
+import { createStore } from 'redux'; 
+import { Provider } from 'react-redux';
 import './index.css';
+
+const gameStore = createStore (
+  allReducers
+);
+
 
 function App() {
   return (
-    <div className="App">
-        <h2>Wagon</h2>
-        <GameArea/>
-    </div>
+    <Provider store={gameStore}>
+      <div className="App">
+          <h2>Wagon</h2>
+          <GameArea/>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/reducers/index.jsx
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+
+const allReducers = combineReducers ({
+
+    //Reducers names
+    //Nome dos reducers
+    
+})
+
+export default allReducers;
